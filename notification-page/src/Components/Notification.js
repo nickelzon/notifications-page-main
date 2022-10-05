3cda20f36c1714f26e65e653ee24ca2244961ab6
alnickelzon <tanyaharthart007@gmail.com>
2022-10-06 02:38:40 +0800

    arranged code from notification component

--- notification-page/src/Components/Notification.js
+++ notification-page/src/Components/Notification.js
@@ -4,7 +4,20 @@
 const Notification = ({ notifications, forRead }) => {
   return (
     <>
-        {notifications.map((data) => (<div key={data.id} onDoubleClick={() => forRead(data.id)} className={data.read ? "notification read" : "notification"}><img src={data.urlphoto} alt={data.urlphoto}/><strong>{data.name}</strong> {data.activity} <strong>{data.group}</strong>{data.displayphoto ? <img src={data.displayphoto} alt={data.displayphoto} className="displayphoto"></img> : ''}{data.message ? <div className="chat">{data.message}</div> : ''}<div className={!data.read ?"badge-circle-red" : ""}></div><br/><span>{data.time}</span></div>))}
+        {notifications.map((data) => (<div key={data.id} onDoubleClick={() => forRead(data.id)} className={data.read ? "notification read" : "notification"}>
+
+          <img src={data.urlphoto} alt={data.urlphoto}/>
+
+          <strong>{data.name}</strong>
+
+           {data.activity} 
+
+           <strong>{data.group}</strong>
+
+           {data.displayphoto ? <img src={data.displayphoto} alt={data.displayphoto} className="displayphoto"></img> : ''}
+           
+           {data.message ? <div className="chat">{data.message}</div> : ''}
+           <div className={!data.read ?"badge-circle-red" : ""}></div><br/><span>{data.time}</span></div>))}
     </>
   )
 }
